1869f0dd5cd2805d02e0346073805a309d8fdcfb
shartbubblez <kajban87@gmail.com>
2025-10-20 17:01:16 +0200

    Update welcum.py
    ny fil till gruppen

--- welcum.py
+++ welcum.py
@@ -1,3 +1,3 @@
 welcum = input("Hello and welcum to this git. How cum you're here?")
 print(f"{welcum}??? Haha, that's retarded.")
-print("Now go do something useful...")
+print("Now go do something useful for the group...")
